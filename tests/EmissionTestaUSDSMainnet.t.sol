940bc54eced2922215314378f1f631e167492b87
Rozengarden <github@rozengarden.eu>
2024-10-08 23:12:00 +0200

    feat: aUSDS distribution

---
+++ tests/EmissionTestaUSDSMainnet.t.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {Test} from 'forge-std/Test.sol';
+import {IERC20} from 'forge-std/interfaces/IERC20.sol';
+import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {IAaveIncentivesController} from '../src/interfaces/IAaveIncentivesController.sol';
+
+import {IEmissionManager, ITransferStrategyBase, RewardsDataTypes, IEACAggregatorProxy} from '../src/interfaces/IEmissionManager.sol';
+import {BaseTest} from './utils/BaseTest.sol';
+
+contract EmissionTestaUSDSEthereum is BaseTest {
+  /// @dev Used to simplify the definition of a program of emissions
+  /// @param asset The asset on which to put reward on, usually Aave aTokens or vTokens (variable debt tokens)
+  /// @param emission Total emission of a `reward` token during the whole distribution duration defined
+  /// E.g. With an emission of 10_000 MATICX tokens during 1 month, an emission of 50% for variableDebtPolWMATIC would be
+  /// 10_000 * 1e18 * 50% / 30 days in seconds = 1_000 * 1e18 / 2_592_000 = ~ 0.0003858 * 1e18 MATICX per second
+  struct EmissionPerAsset {
+    address asset;
+    uint256 emission;
+  }
+
+  address constant EMISSION_ADMIN = 0xac140648435d03f784879cd789130F22Ef588Fcd; // aci
+  address constant REWARD_ASSET = 0x32a6268f9Ba3642Dda7892aDd74f1D34469A4259; //hardcoded as lib not up to date
+  address constant USDS_A_TOKEN = REWARD_ASSET; //hardcoded as lib not up to date
+  IEACAggregatorProxy constant REWARD_ORACLE =
+    IEACAggregatorProxy(0x4F01b76391A05d32B20FA2d05dD5963eE8db20E6);
+
+  /// @dev already deployed and configured for the both the MATICX asset and the 0x0c54a0BCCF5079478a144dBae1AFcb4FEdf7b263
+  /// EMISSION_ADMIN
+  ITransferStrategyBase constant TRANSFER_STRATEGY =
+    ITransferStrategyBase(0x4fDB95C607EDe09A548F60685b56C034992B194a);
+
+  uint256 constant TOTAL_DISTRIBUTION = 50_000 ether; // 10'000 MATICX/month, 6 months
+  uint88 constant DURATION_DISTRIBUTION = 7 days;
+
+  address asUSDS_WHALE = 0x230F86Fa0357fEB4e9F5043986383CFfb3DAB2bc;
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('mainnet'), 20923316);
+  }
+
+  function test_activation() public {
+    vm.startPrank(EMISSION_ADMIN);
+    /// @dev IMPORTANT!!
+    /// The emissions admin should have REWARD_ASSET funds, and have approved the TOTAL_DISTRIBUTION
+    /// amount to the transfer strategy. If not, REWARDS WILL ACCRUE FINE AFTER `configureAssets()`, BUT THEY
+    /// WILL NOT BE CLAIMABLE UNTIL THERE IS FUNDS AND ALLOWANCE.
+    /// It is possible to approve less than TOTAL_DISTRIBUTION and doing it progressively over time as users
+    /// accrue more, but that is a decision of the emission's admin
+    IERC20(REWARD_ASSET).approve(address(TRANSFER_STRATEGY), TOTAL_DISTRIBUTION);
+
+    IEmissionManager(AaveV3Ethereum.EMISSION_MANAGER).configureAssets(_getAssetConfigs());
+
+    emit log_named_bytes(
+      'calldata to submit from Gnosis Safe',
+      abi.encodeWithSelector(
+        IEmissionManager(AaveV3Ethereum.EMISSION_MANAGER).configureAssets.selector,
+        _getAssetConfigs()
+      )
+    );
+
+    vm.stopPrank();
+
+    //vm.startPrank(asUSDS_WHALE);
+    //IERC20(REWARD_ASSET).transfer(EMISSION_ADMIN, 50_000 ether);
+
+    //vm.stopPrank();
+    _testClaimRewardsForWhale(asUSDS_WHALE, USDS_A_TOKEN, TOTAL_DISTRIBUTION);
+  }
+
+  function _testClaimRewardsForWhale(
+    address whale,
+    address asset,
+    uint256 expectedReward
+  ) internal {
+    vm.startPrank(whale);
+
+    vm.warp(block.timestamp + 15 days);
+
+    address[] memory assets = new address[](1);
+    assets[0] = asset;
+
+    uint256 balanceBefore = IERC20(REWARD_ASSET).balanceOf(whale);
+
+    IAaveIncentivesController(AaveV3Ethereum.DEFAULT_INCENTIVES_CONTROLLER).claimRewards(
+      assets,
+      type(uint256).max,
+      whale,
+      REWARD_ASSET
+    );
+
+    uint256 balanceAfter = IERC20(REWARD_ASSET).balanceOf(whale);
+
+    uint256 deviationAccepted = expectedReward; // Approx estimated rewards with current emission in 1 month
+    assertApproxEqAbs(
+      balanceBefore,
+      balanceAfter,
+      deviationAccepted,
+      'Invalid delta on claimed rewards'
+    );
+
+    vm.stopPrank();
+  }
+
+  function _getAssetConfigs() internal view returns (RewardsDataTypes.RewardsConfigInput[] memory) {
+    uint32 distributionEnd = uint32(block.timestamp + DURATION_DISTRIBUTION);
+
+    EmissionPerAsset[] memory emissionsPerAsset = _getEmissionsPerAsset();
+
+    RewardsDataTypes.RewardsConfigInput[]
+      memory configs = new RewardsDataTypes.RewardsConfigInput[](emissionsPerAsset.length);
+    for (uint256 i = 0; i < emissionsPerAsset.length; i++) {
+      configs[i] = RewardsDataTypes.RewardsConfigInput({
+        emissionPerSecond: _toUint88(emissionsPerAsset[i].emission / DURATION_DISTRIBUTION),
+        totalSupply: 0, // IMPORTANT this will not be taken into account by the contracts, so 0 is fine
+        distributionEnd: distributionEnd,
+        asset: emissionsPerAsset[i].asset,
+        reward: REWARD_ASSET,
+        transferStrategy: TRANSFER_STRATEGY,
+        rewardOracle: REWARD_ORACLE
+      });
+    }
+
+    return configs;
+  }
+
+  function _getEmissionsPerAsset() internal pure returns (EmissionPerAsset[] memory) {
+    EmissionPerAsset[] memory emissionsPerAsset = new EmissionPerAsset[](1);
+    emissionsPerAsset[0] = EmissionPerAsset({
+      asset: USDS_A_TOKEN,
+      emission: TOTAL_DISTRIBUTION // 100% of the distribution
+    });
+
+    uint256 totalDistribution;
+    for (uint256 i = 0; i < emissionsPerAsset.length; i++) {
+      totalDistribution += emissionsPerAsset[i].emission;
+    }
+    require(totalDistribution == TOTAL_DISTRIBUTION, 'INVALID_SUM_OF_EMISSIONS');
+
+    return emissionsPerAsset;
+  }
+
+  function _toUint88(uint256 value) internal pure returns (uint88) {
+    require(value <= type(uint88).max, "SafeCast: value doesn't fit in 88 bits");
+    return uint88(value);
+  }
+}
